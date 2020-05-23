45da12c3be02cdc6641a04ce2ad47bb1f5c520d3
Jarred Eagley <jarred.eagley@hotmail.com>
2020-05-22 19:19:48 -0700

    Initialized debug arrow rendering class

---
+++ Munwalk/MW_DebugArrow.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Munwalk
+{
+    /// <summary>
+    /// Class to make drawing debug arrows a bit easier.
+    /// </summary>
+    public class MW_DebugArrowGenerator
+    {
+
+    }
+}
